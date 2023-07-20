70cf3021961e32977b3772e6ae5460a3a078b62b
DableUTeeF <palm22180@gmail.com>
2023-07-20 09:49:45 +0700

    Now save only 1 epoch

--- re_extract.py
+++ re_extract.py
@@ -3,8 +3,8 @@ import os
 
 
 if __name__ == '__main__':
-    for file in os.listdir('outs/ori'):
-        text = open(f'outs/ori/{file}', encoding='ISO-8859-1').read()
+    for file in os.listdir('slurms'):
+        text = open(f'slurms/{file}', encoding='ISO-8859-1').read()
         matches = re.findall(r"{'ev.*?}", text, re.DOTALL)
         with open(f'outs/fixed/{file.replace(".out", "-fixed.out")}', 'w') as wr:
             for match in matches:
